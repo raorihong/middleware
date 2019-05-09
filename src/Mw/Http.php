46d7317e94f10328e67d8c059d8b4782b23aecde
Frank <raorihong@126.com>
2019-05-09 11:08:41 +0800

    upgrade:add function getResponseData()

--- src/Mw/Http.php
+++ src/Mw/Http.php
@@ -10,17 +10,42 @@ namespace Mw;
 
 
 use Curl\Curl;
+use Exception;
 
 class Http extends Curl
 {
+    private $responseData = null;
+
     public function __construct($base_url = null)
     {
         parent::__construct($base_url);
         $logicAuthBearerCode = getenv('LOGIC_AUTH_BEARER_CODE');
         $logicHost = getenv('LOGIC_HOST');
         $logicApiVersion = getenv('LOGIC_API_VERSION');
-        if($logicAuthBearerCode) $this->setHeader('Authorization', 'Bearer '.$logicAuthBearerCode);
+        if($logicAuthBearerCode){
+            $this->setHeader('Authorization', 'Bearer '.$logicAuthBearerCode);
+            $this->setUserAgent($logicAuthBearerCode);
+        }
         if($logicHost) $this->url = $logicHost;
         if($logicApiVersion && $logicHost) $this->url .= $logicApiVersion;
+    }
+
+    /**
+     * get response json data object
+     * @return null
+     */
+    public function getResponseData()
+    {
+        if($this->getHttpStatusCode() === 200){
+            try{
+                $resp = json_decode($this->getResponse());
+                if(isset($resp->data)){
+                    $this->responseData = $resp->data;
+                }
+            }catch (Exception $e){
+
+            }
+        }
+        return $this->responseData;
     }
 }
